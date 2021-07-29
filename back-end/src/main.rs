2e18fee8d32d12dcf4ca2616d77b73eeb63ccefb
CynicalCode21 <darrencaldwell00@outlook.com>
2021-07-29 14:15:35 +0100

    feat: added actix web to repo

--- back-end/src/main.rs
+++ back-end/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, auto updater!");
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/{id}/{name}/index.html")]
+async fn index(path: web::Path<(u32, String)>) -> impl Responder {
+    let (id, name) = path.into_inner();
+    format!("Hello {}! id:{}", name, id)
+}
+
+#[get("/ping")]
+async fn ping() -> impl Responder {
+    format!("pong")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(ping).service(index))
+        
+        .bind("0.0.0.0:21452")?
+        .run()
+        .await
 }
